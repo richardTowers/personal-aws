d046107898b98a4baf09c65e097074cd7987117c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2022-01-08 16:25:48 +0000

    ARP Investigation Terraform

---
+++ arp-investigation/ec2.tf
@@ -0,0 +1,72 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "arp-investigation"
+  public_key = file("~/.ssh/arp-investigation.pub")
+}
+
+resource "aws_instance" "foo" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.this.key_name
+
+  subnet_id                   = aws_subnet.this.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [
+    aws_security_group.foo.id,
+    aws_security_group.allow_ssh.id,
+  ]
+
+  user_data = join("\n", [
+    "#cloud-config",
+    yamlencode({
+      packages = [ "net-tools" ]
+    })
+  ])
+
+  tags = {
+    Name = "Foo"
+  }
+}
+
+resource "aws_instance" "bar" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.this.key_name
+
+  subnet_id                   = aws_subnet.this.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [
+    aws_security_group.bar.id,
+    aws_security_group.allow_ssh.id,
+  ]
+
+  user_data = join("\n", [
+    "#cloud-config",
+    yamlencode({
+      packages = [ "net-tools", "apache2" ]
+      write_files = [{
+        content = "Hello from Bar!"
+        path    = "/var/www/html/index.html"
+      }]
+    })
+  ])
+
+  tags = {
+    Name = "Bar"
+  }
+}
---
+++ arp-investigation/security-groups.tf
@@ -0,0 +1,41 @@
+data "http" "ip" {
+  url = "https://ifconfig.me"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    cidr_blocks = ["${data.http.ip.body}/32"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+}
+
+resource "aws_security_group" "foo" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group" "bar" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "bar_from_foo" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.bar.id
+  source_security_group_id = aws_security_group.foo.id
+}
+
+resource "aws_security_group_rule" "foo_to_bar" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.foo.id
+  source_security_group_id = aws_security_group.bar.id
+}
+
