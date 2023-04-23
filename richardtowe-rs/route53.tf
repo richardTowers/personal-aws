7bbe8587a5b18e49edcf63186b875bf1e9007cd0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2023-04-23 20:58:22 +0100

    Import missing records for richardtowe.rs

--- richardtowe-rs/route53.tf
+++ richardtowe-rs/route53.tf
@@ -2,6 +2,43 @@ resource "aws_route53_zone" "this" {
   name = "richardtowe.rs"
 }
 
+resource "aws_route53_record" "apex_a" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = ""
+  type    = "A"
+  ttl     = 30
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+  ]
+}
+
+resource "aws_route53_record" "apex_aaaa" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = ""
+  type    = "AAAA"
+  ttl     = 30
+  records = [
+    "2606:50c0:8000::153",
+    "2606:50c0:8001::153",
+    "2606:50c0:8002::153",
+    "2606:50c0:8003::153",
+  ]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "www"
+  type    = "A"
+  ttl     = 30
+  records = [
+    "192.30.252.153",
+    "192.30.252.154",
+  ]
+}
+
 resource "aws_route53_record" "learn_to_pong" {
   zone_id = aws_route53_zone.this.zone_id
   name    = "learn-to-pong"
